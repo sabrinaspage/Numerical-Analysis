706ec68d5e4bc13bbc9a9e2f34b02e9365b00950
sabrinaspage <sabreyes01@gmail.com>
2021-03-11 20:52:50 -0500

    update import names

--- solutions/homework_2/condition_a.py
+++ solutions/homework_2/condition_a.py
@@ -1,7 +1,8 @@
 import numpy as np
 from scipy.linalg import lu
-import operations
+import solutions.homework_2.operations as operations
 
+# Condition function is built with the methods below
 
 def main():
     vector_c = np.array([1, 1, 1])
@@ -23,26 +24,24 @@ diagonal elements, and U upper triangle for A1 using Scipy \n")
     print(A1_norm)
 
     print("\nSolution to A1 backward substitution \n")
-    print(operations.backward(np.transpose(u1), vector_c))
-
     A1_backward_v = operations.backward(np.transpose(u1), vector_c)
+    print(A1_backward_v)
 
     print("\nSolution to A1 forward substitution \n")
-    print(operations.forward(np.transpose(l1), vector_c))
-
     A1_forward_y = operations.forward(np.transpose(l1), vector_c)
+    print(A1_forward_y)
 
     print("\nSolutions for z vector ")
-    print(np.linalg.solve(A1, A1_forward_y))
     A1_z_vector = np.linalg.solve(A1, A1_forward_y)
-    A1_norm_of_y = np.linalg.norm(A1_forward_y)
-    A1_norm_of_z = np.linalg.norm(A1_z_vector)
+    print(A1_z_vector)
 
     print("\nA1 Inverse")
+    A1_norm_of_y = np.linalg.norm(A1_forward_y)
+    A1_norm_of_z = np.linalg.norm(A1_z_vector)
     A1_inverse_norm = A1_norm_of_y/A1_norm_of_z
     print(A1_inverse_norm)
 
-    print("\nA1 Condition number")
+    print("\nA1 Estimate Condition number")
     A1_cond_num = A1_norm * A1_inverse_norm
     print(A1_cond_num)
 
@@ -54,11 +53,11 @@ diagonal elements, and U upper triangle for A1 using Scipy \n")
 
     p2, l2, u2 = lu(A2)
 
-    print("\n/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
-    print("\n/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
+    print("\n/////////////////////////////////////////////////////////////////////")
+    print("\n/////////////////////////////////////////////////////////////////////")
 
     print("\nPermutation matrix, lower triangle with unit diagonal \
-    diagonal elements, and U upper triangle for A2 using Scipy \n")
+diagonal elements, and U upper triangle for A2 using Scipy \n")
     print(p2, l2, u2, sep="\n")
 
     print("\nA2 norm \n")
@@ -66,27 +65,24 @@ diagonal elements, and U upper triangle for A1 using Scipy \n")
     print(A1_norm)
 
     print("\nSolution to A2 backward substitution \n")
-    print(operations.backward(np.transpose(u2), vector_c))
-
     A2_backward_v = operations.backward(np.transpose(u2), vector_c)
+    print(A2_backward_v)
 
     print("\nSolution to A2 forward substitution \n")
-    print(operations.forward(np.transpose(l2), vector_c))
-
     A2_forward_y = operations.forward(np.transpose(l2), vector_c)
+    print(A2_forward_y)
 
     print("\nSolutions for z vector ")
-    print(np.linalg.solve(A2, A2_forward_y))
     A2_z_vector = np.linalg.solve(A2, A2_forward_y)
-    A2_z_vector = np.linalg.solve(A2, A2_forward_y)
-    A2_norm_of_y = np.linalg.norm(A2_forward_y)
-    A2_norm_of_z = np.linalg.norm(A2_z_vector)
+    print(A1_z_vector)
 
     print("\nA2 Inverse")
+    A2_norm_of_y = np.linalg.norm(A2_forward_y)
+    A2_norm_of_z = np.linalg.norm(A2_z_vector)
     A2_inverse_norm = A2_norm_of_y/A2_norm_of_z
     print(A2_inverse_norm)
 
-    print("\nA2 Condition number")
+    print("\nA2 Estimate Condition number")
     A2_cond_num = A2_norm * A2_inverse_norm
     print(A2_cond_num)
 
--- solutions/homework_2/scaling.py
+++ solutions/homework_2/scaling.py
@@ -1,7 +1,7 @@
 import scipy
 import numpy as np
 import random
-import operations
+import solutions.homework_2.operations as operations
 
 
 def main():
